36316620f26a657d821d333d67c7c9fad7ae2977
Andrzej Borzęcki <andrew.borzecki@gmail.com>
2017-03-02 19:49:08 +0100

    Refactored StacksDemo class

--- src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
+++ src/test/java/edu/kis/vh/stacks/demo/StacksDemo.java
@@ -9,27 +9,42 @@ class StacksDemo {
     public static void main(String[] args) {
         DefaultStacksFactory factory = new DefaultStacksFactory();
 
+        testStacks(factory);
+
+    }
+
+    private static void testStacks(DefaultStacksFactory factory) {
         Stack[] Stacks = {factory.getStandardStack(), factory.getFalseStack(), factory.getFIFOStack(),
                 factory.getHanoiStack()};
 
+        fillStacks(Stacks);
+        fillHanoiStack(Stacks[3]);
+        printStacksValues(Stacks);
+
+        //niepotrzebne zlamanie linii po ""
+        System.out.println("total rejected is " + ((StackHanoi) Stacks[3]).reportRejected());
+    }
+
+    private static void fillStacks(Stack[] stacks) {
         for (int i = 1; i < 15; i++)
             for (int j = 0; j < 3; j++)
-                Stacks[j].push(i);
+                stacks[j].push(i);
+    }
 
+    private static void fillHanoiStack(Stack stack) {
         //o jeden tab za duzo w liniach 20-28
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < 15; i++)
-            Stacks[3].push(rn.nextInt(20));
+            stack.push(rn.nextInt(20));
+    }
 
-        for (int i = 0; i < Stacks.length; i++) {
-            while (!Stacks[i].isEmpty())
-                System.out.print(Stacks[i].pop() + "  ");
+    private static void printStacksValues(Stack[] stacks) {
+        for (int i = 0; i < stacks.length; i++) {
+            while (!stacks[i].isEmpty())
+                System.out.print(stacks[i].pop() + "  ");
             //linia 28 przyklejona do lini 26 utrudniajac zrozumienie kodu, przeniesiona nizej i poprawione wciecie (1 tab za duzo)
             System.out.println();
         }
-        //niepotrzebne zlamanie linii po ""
-        System.out.println("total rejected is " + ((StackHanoi) Stacks[3]).reportRejected());
-
     }
 
 }
