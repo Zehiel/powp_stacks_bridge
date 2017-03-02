49795aef6ef3b428573de7005ec6dd402fc61c17
Andrzej Borzęcki <andrew.borzecki@gmail.com>
2017-03-02 19:09:52 +0100

    Futher changes in access modificators, generated getters and setters for all non-public fields in Stack class

--- src/main/java/edu/kis/vh/stacks/Stack.java
+++ src/main/java/edu/kis/vh/stacks/Stack.java
@@ -3,13 +3,13 @@ package edu.kis.vh.stacks;
 public class Stack {
 
     private static final int EMPTY_STACK_INDICATOR = -1;
-    private int[] items = new int[12];
+    private int[] items = new int[12]; //bezposrednie odniesienia do zmiennej zostaly zastapione wywolaniem getterow/setterow
 
     private int total = EMPTY_STACK_INDICATOR;
 
     public void push(int i) {
         if (!isFull())
-            items[++total] = i;
+            getItems()[++total] = i;
     }
 
     public boolean isEmpty() {
@@ -17,19 +17,19 @@ public class Stack {
     }
 
     public boolean isFull() {
-        return total == items.length - 1;
+        return total == getItems().length - 1;
     }
 
     public int top() {
         if (isEmpty())
             return EMPTY_STACK_INDICATOR;
-        return items[total];
+        return getItems()[total];
     }
 
     public int pop() {
         if (isEmpty())
             return EMPTY_STACK_INDICATOR;
-        return items[total--];
+        return getItems()[total--];
     }
 
     //dzialanie enkapsulacji pola w intelij uniemozliwilo automatyczna generacje gettera dla tego przykladu
@@ -37,4 +37,15 @@ public class Stack {
         return total;
     }
 
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int[] getItems() {
+        return items;
+    }
+
+    public void setItems(int[] items) {
+        this.items = items;
+    }
 }
