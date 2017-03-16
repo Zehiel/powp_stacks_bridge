e56e81ebe3f827f94f0a2e48434b56a6a9c5b929
Andrzej Borzęcki <andrew.borzecki@gmail.com>
2017-03-16 14:48:37 +0100

    Added StackArray class

---
+++ src/main/java/edu/kis/vh/stacks/StackArray.java
@@ -0,0 +1,74 @@
+package edu.kis.vh.stacks;
+
+/**
+ * Created by grusz on 16.03.2017.
+ */
+public class StackArray {
+    private static final int EMPTY_STACK_INDICATOR = -1;
+    private int[] items = new int[12]; //bezposrednie odniesienia do zmiennej zostaly zastapione wywolaniem getterow/setterow
+
+    private int total = EMPTY_STACK_INDICATOR;
+
+    /**
+     * puts value on the top of the stack
+     * @param i - value of type integer to be pushed into stack
+     * modifies:items
+     */
+    public void push(int i) {
+        if (!isFull())
+            getItems()[++total] = i;
+    }
+
+    /**
+     * Return boolean indicating if stack is empty
+     * @return boolean - true if stack is empty, false if not
+     * modifies:none
+     */
+    public boolean isEmpty() {
+        return total == EMPTY_STACK_INDICATOR;
+    }
+
+    /**
+     * Returns boolean indicating if stack is full
+     * @return boolean - true if stack is full, false if not
+     * modifies:none
+     */
+    public boolean isFull() {
+        return total == getItems().length - 1;
+    }
+
+    /**
+     * returns value on the top of the stack, doesn't modify stack
+     * @return value on top of the stack
+     * modifies:none
+     */
+    public int top() {
+        //TODO Exception based handling
+        if (isEmpty())
+            return EMPTY_STACK_INDICATOR;
+        return getItems()[total];
+    }
+
+    /**
+     * takes value on the top of the stack, modifies stack
+     * @return value on top of the stack
+     * modifies:items
+     */
+    public int pop() {
+        //TODO Exception based handling
+        if (isEmpty())
+            return EMPTY_STACK_INDICATOR;
+        return getItems()[total--];
+    }
+
+    //dzialanie enkapsulacji pola w intelij uniemozliwilo automatyczna generacje gettera dla tego przykladu
+    public int getTotal() {
+        return total;
+    }
+
+
+    public int[] getItems() {
+        return items;
+    }
+
+}
